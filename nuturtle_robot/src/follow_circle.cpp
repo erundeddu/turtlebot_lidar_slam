adad4ba26f17cae0b9b8e62318cb2037fb5bd35e
Edoardo Rundeddu <edoardorundeddu2021@u.northwestern.edu>
2021-02-09 16:47:18 -0600

    changed test for encoder to check for array size of incoming message fields

--- nuturtle_robot/src/follow_circle.cpp
+++ nuturtle_robot/src/follow_circle.cpp
@@ -4,21 +4,7 @@
 /// PUBLISHES:
 
 /// SUBSCRIBES:
-///     cmd_vel (geometry_msgs/Twist): commanded twist
-///		sensor_data (nuturtlebot/SensorData): wheel angles encoder data
+
 ///	SERVICES:
+///		
 
-#include <ros/ros.h>
-#include <sensor_msgs/JointState.h>
-#include <geometry_msgs/Twist.h>
-#include <nuturtlebot/WheelCommands.h>
-#include <nuturtlebot/SensorData.h>
-#include <string>
-#include "rigid2d/diff_drive.hpp"
-#include "rigid2d/rigid2d.hpp"
-#include "rigid2d/set_pose.h"
-
-int main(int argc, char** argv)
-{
-	using namespace rigid2d;
-	
