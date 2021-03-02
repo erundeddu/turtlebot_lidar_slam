7a094bec27708ed52ec3c5452b0e87617ef7aa5e
Edoardo Rundeddu <edoardorundeddu2021@u.northwestern.edu>
2021-03-02 10:57:03 -0600

    Doxygen comments for nurtlesim

--- nurtlesim/include/nurtlesim/multivariate.hpp
+++ nurtlesim/include/nurtlesim/multivariate.hpp
@@ -7,18 +7,26 @@
 #include <random>
 #include <vector>
 
+/// \brief a zero-mean multivariate Gaussian distribution
 class Multivar
 {
 private:
-	arma::Mat<double> Q;
-	arma::Mat<double> L;
+	// distribution variance matrix
+	arma::Mat<double> Q; 
+	// lower Cholesky decomposition of the variance matrix Q 
+	arma::Mat<double> L; 
+	// array of one-variable normal distribution objects 
 	std::vector<std::normal_distribution<>> dist_arr;
 	
 	std::mt19937 & get_random();
 
 public:
+	/// \brief constructs a zero-mean multivariate Gaussian distribution from a covariance matrix
+	/// \param cov - the covariance matrix of the distribution
 	Multivar(arma::Mat<double> & cov);
 	
+	/// \brief draws a vector from the multivariate Gaussian distribution that was initialized
+	/// \return a vector of multivariate Gaussian random entries
 	std::vector<double> draw();
 };
 
