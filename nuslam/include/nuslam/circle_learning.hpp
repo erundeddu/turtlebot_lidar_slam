47ad493a403e2672262cf67d1ebd5cdc908790e3
Edoardo Rundeddu <edoardorundeddu2021@u.northwestern.edu>
2021-03-05 21:50:50 -0600

    using function templates for mean and stdev functions

--- nuslam/include/nuslam/circle_learning.hpp
+++ nuslam/include/nuslam/circle_learning.hpp
@@ -8,12 +8,23 @@
 
 namespace nuslam
 {
+	/// \brief structure to represent a circle on a plane
+	struct Circle
+	{
+		// x coordinate of circle center
+		double x = 0.0;
+		// y coordinate of circle center
+		double y = 0.0;
+		// radius of circle
+		double r = 1.0;
+	};	
+	
 	/// \brief cluster an array of laser ranges given a distance threshold and discard clusters with a low number of elements
 	/// \param ranges - array of range data
 	/// \param thresh - threshold above which adjacent range data belong to different clusters
 	/// \param min_n - minimum number of data points in a cluster to be retained
 	/// \return a vector of vectors, where each vector represents a different cluster and contains the indices of the range data array belonging to the cluster
-	std::vector<std::vector<int>> cluster_ranges(std::vector<float> & ranges, double thresh, int min_n);
+	std::vector<std::vector<int>> cluster_ranges(const std::vector<float> & ranges, double thresh, int min_n);
 	
 	/// \brief classifies a cluster of points into Circle or Not Circle
 	/// \param ranges - array of range data
@@ -23,24 +34,31 @@ namespace nuslam
 	/// \param max_mean - maximum value of angle mean (in radians) for Circle
 	/// \param max_std - maximum value of angle standard deviation (in radians) for Circle
 	/// \return true if Circle, false if Not Circle
-	bool is_circle(std::vector<float> & ranges, std::vector<int> & cluster, double dtheta, float min_mean, float max_mean, float max_std);
+	bool is_circle(const std::vector<float> & ranges, const std::vector<int> & cluster, double dtheta, float min_mean, float max_mean, float max_std);
 	
-	/// \brief computes the mean of a vector of float
-	/// \param v - a vector of float
+	/// \brief computes the mean of a vector
+	/// \param v - a vector
 	/// \return mean of v
-	float mean(std::vector<float> & v);
+	template <typename T>
+	T mean(const std::vector<T> & v);
 	
-	/// \brief computes the standard deviation of a vector of float
-	/// \param v - a vector of float
+	/// \brief computes the standard deviation of a vector
+	/// \param v - a vector
 	/// \return standard deviation of v
-	float stdev(std::vector<float> & v);
+	template <typename T>
+	T stdev(const std::vector<T> & v);
 	
 	/// \brief converts range data in a cluster into relative xy coordinates
 	/// \param ranges - array of range data
 	/// \param cluster - array of indices relative to range data
 	/// \param dtheta - angular increment between adjacent measurements
 	/// \return vector of Vector2D representing relative xy coordinates of data points
-	std::vector<rigid2d::Vector2D> range2xy(std::vector<float> & ranges, std::vector<int> & cluster, double dtheta);
+	std::vector<rigid2d::Vector2D> range2xy(const std::vector<float> & ranges, const std::vector<int> & cluster, double dtheta);
+	
+	/// \brief fits a circle to data points
+	/// \param pts - array of xy coordinates of data points
+	/// \return fitted circle in center coordinates + radius form
+	Circle fit_circle(const std::vector<rigid2d::Vector2D> & pts);
 }
 
 #endif
--- nuslam/src/circle_learning.cpp
+++ nuslam/src/circle_learning.cpp
@@ -2,10 +2,11 @@
 #include "rigid2d/rigid2d.hpp"
 #include <vector>
 #include <cmath>
+#include <armadillo>
 
 namespace nuslam 
 {	
-	std::vector<std::vector<int>> cluster_ranges(std::vector<float> & ranges, double thresh, int min_n)
+	std::vector<std::vector<int>> cluster_ranges(const std::vector<float> & ranges, double thresh, int min_n)
 	{
 		std::vector<std::vector<int>> clusters_list;
 		std::vector<int> cluster;
@@ -56,7 +57,7 @@ namespace nuslam
 		return clusters_list;
 	}
 	
-	bool is_circle(std::vector<float> & ranges, std::vector<int> & cluster, double dtheta, float min_mean, float max_mean, float max_std)
+	bool is_circle(const std::vector<float> & ranges, const std::vector<int> & cluster, double dtheta, float min_mean, float max_mean, float max_std)
 	{
 		using namespace rigid2d;
 		
@@ -86,9 +87,10 @@ namespace nuslam
 		}
 	}	
 	
-	float mean(std::vector<float> & v)
+	template <typename T>
+	T mean(const std::vector<T> & v)
 	{
-		float sum = 0.0;
+		T sum = 0.0;
 		for (std::size_t i=0; i<v.size(); ++i)
 		{
 			sum += v[i];
@@ -96,10 +98,11 @@ namespace nuslam
 		return (sum/v.size());
 	}
 	
-	float stdev(std::vector<float> & v)
+	template <typename T>
+	T stdev(const std::vector<T> & v)
 	{
-		float avg = mean(v);
-		float ssq = 0.0;
+		T avg = mean(v);
+		T ssq = 0.0;
 		for (std::size_t i=0; i<v.size(); ++i)
 		{
 			ssq += ((v[i] - avg)*(v[i] - avg));
@@ -107,7 +110,7 @@ namespace nuslam
 		return (sqrt(ssq)/(v.size()-1));
 	}
 	
-	std::vector<rigid2d::Vector2D> range2xy(std::vector<float> & ranges, std::vector<int> & cluster, double dtheta)
+	std::vector<rigid2d::Vector2D> range2xy(const std::vector<float> & ranges, const std::vector<int> & cluster, double dtheta)
 	{
 		using namespace rigid2d;
 		std::vector<rigid2d::Vector2D> v_xy;
@@ -117,5 +120,36 @@ namespace nuslam
 			v_xy.push_back(v);
 		}
 		return v_xy;
+	}
+	
+	Circle fit_circle(const std::vector<rigid2d::Vector2D> & pts)
+	{
+		using namespace rigid2d;
+		
+		Vector2D avg_pt;
+		for (std::size_t i=0; i<pts.size(); ++i)
+		{
+			avg_pt += pts[i];
+		}
+		avg_pt *= (1.0/pts.size());
+		
+		std::vector<double> x;
+		std::vector<double> y;
+		std::vector<double> z;
+		for (std::size_t i=0; i<pts.size(); ++i)
+		{
+			double xi = pts[i].x-avg_pt.x;
+			double yi = pts[i].y-avg_pt.y;
+			double zi = xi*xi + yi*yi;
+			x.push_back(xi);
+			y.push_back(yi);
+			z.push_back(zi);
+		}	
+		double z_avg = mean(z);
+		
+		arma::Col<double> xcol(x);
+		arma::Col<double> ycol(y);
+		arma::Col<double> zcol(z);
+		arma::Mat<double> Z = arma::join_rows(zcol,xcol,ycol,arma::ones(pts.size(),1));
 	}
 }
